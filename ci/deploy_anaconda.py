c093f92e4666d5ea3bc2f8b1347b8ace3a6c5621
Chilipp <p.sommer@posteo.de>
2018-12-06 00:42:39 +0100

    Fixed python version for deploy

--- ci/deploy_anaconda.py
+++ ci/deploy_anaconda.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import os
+import re
 import conda_build.api
 import subprocess as spr
 fnames = list(conda_build.api.get_output_file_paths('conda-recipe'))
+py_patt = re.compile('py\d\d')
+repl = 'py' + os.getenv('PYTHON_VERSION').replace('.', '')
+fnames = [py_patt.sub(repl, f) for f in fnames]
 if os.getenv("TRAVIS") == "true":
     branch = os.getenv("TRAVIS_BRANCH")
 else:
